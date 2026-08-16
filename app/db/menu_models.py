fa749e86def12544ed99bd2669d541e9a048b2e8
User <user@Users-iMac.lan>
2026-08-16 00:56:33 -0400

    make name and price nullable=False and make description Text and nullable=False in menu_models.py

--- app/db/menu_models.py
+++ app/db/menu_models.py
@@ -1,10 +1,10 @@
-from sqlalchemy import Column, ForeignKey, Numeric, String
+from sqlalchemy import Column, ForeignKey, Numeric, String, Text
 from app.db.base import Base
 
 class MenuItem(Base):
     __tablename__ = "menu_items"
 
     id = Column(Numeric, primary_key=True, index=True)
-    name = Column(String, index=True)
+    name = Column(String, index=True, nullable=False)
     description = Column(String)
-    price = Column(Numeric(precision=6, scale=2))
+    price = Column(Numeric(precision=6, scale=2), nullable=False)
--- app/db/user_models.py
+++ app/db/user_models.py
@@ -5,6 +5,6 @@ from uuid import UUID
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(UUID, primary_key=True, default=uuid.uuid4)
+    id = Column(UUID, primary_key=True, index=True, default=uuid.uuid4)
     name = Column(String, nullable=False)
     role = Column(String, nullable=False)
