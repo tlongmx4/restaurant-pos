35b762d0ecfaebe87f1986621de47c0b61afebca
User <user@Users-iMac.lan>
2026-08-16 00:28:43 -0400

    add indexing to id and name columns in menu_models.py

--- app/db/menu_models.py
+++ app/db/menu_models.py
@@ -4,7 +4,7 @@ from app.db.base import Base
 class MenuItem(Base):
     __tablename__ = "menu_items"
 
-    id = Column(Numeric, primary_key=True)
-    name = Column(String)
+    id = Column(Numeric, primary_key=True, index=True)
+    name = Column(String, index=True)
     description = Column(String)
     price = Column(Numeric(precision=6, scale=2))
